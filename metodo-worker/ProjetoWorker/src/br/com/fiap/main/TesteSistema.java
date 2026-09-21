0a427d31aea984a2cd84a88bba1a6feb6e7f92e4
dev-luisfernandes <luisfelipefernandes.corp@gmail.com>
2026-09-21 11:46:41 -0300

    s

--- metodo-worker/ProjetoWorker/src/br/com/fiap/main/TesteSistema.java
+++
@@ -1,4 +0,0 @@
-package br.com.fiap.main;
-
-public class testeSistema {
-}
--- services/ViacepServices.java
+++ services/ViacepServices.java
@@ -2,12 +2,18 @@ package br.com.fiap.services;
 
 
 import br.com.fiap.api.Endereco;
+import org.apache.http.client.methods.HttpGet;
 
 public class ViacepServices {
 
     public Endereco getEndereco(String cep){
 
         Endereco endereco = null;
+
+        //request
+        HttpGet re
+        //cliente
+        //response
 
         return endereco;
 
