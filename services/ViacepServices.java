4d142e1de9d54d54b65792354057d939ed164d60
dev-luisfernandes <luisfelipefernandes.corp@gmail.com>
2026-09-21 11:44:01 -0300

    services criado

---
+++ services/ViacepServices.java
@@ -0,0 +1,15 @@
+package br.com.fiap.services;
+
+
+import br.com.fiap.api.Endereco;
+
+public class ViacepServices {
+
+    public Endereco getEndereco(String cep){
+
+        Endereco endereco = null;
+
+        return endereco;
+
+    }
+}
